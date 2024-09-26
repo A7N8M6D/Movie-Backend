4167ae107835a4ce39ba7ff720cd7826ab28bb19
ANMD <nosha3173@gmail.com>
2024-09-26 10:09:53 +0500

    added vercel.json

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ const app=express();
 
 app.use(cors({
     origin:"http://localhost:5173",
-    credentials:true
+    credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }))
 // credentials:true
 
